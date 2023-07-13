ca22b49b2ca704816289c0918ff98514c31fbff6
Francisco Hernández Castillo <48812779+frahercas@users.noreply.github.com>
2023-07-13 16:38:47 -0600

    Add files via upload

---
+++ Basicos de JAVA/A001Variables.java
@@ -0,0 +1,13 @@
+
+public class A001Variables {
+
+	public static void main(String[] args) {
+		byte edad;		//Declaracion de variable
+		edad=25;		//inicalizacion de la variable
+		String name= "francisco"; //Declaracion e inicializacion de una variable en la misma linea
+		System.out.println(edad);
+		System.out.println(name);
+
+	}
+
+}
---
+++ Basicos de JAVA/A002Declaraciones_Operadores.java
@@ -0,0 +1,32 @@
+
+public class A002Declaraciones_Operadores {
+
+	public static void main(String[] args) {
+		int a=5, b=7;
+		System.out.println("OPERADORES ARITMETICOS");
+		int suma=a+b; //Operador suma
+		int resta=a-b;	//Operador Resta
+		int multiplicacion= a*b;	//Operador multiplicacion
+		float x=7,y=2;
+		float division= x/y; 	//Operador division
+		System.out.println("suma igual a "+suma);
+		System.out.println("resta igual a "+resta);
+		System.out.println("multiplicacion igual a "+multiplicacion);
+		System.out.println("division igual a "+division);   //En estos ejemplos concatenamos una cadena de caracteres con el valor de una variable
+		
+		System.out.println("OPERADORES incremento y decremento");
+		int numero= 100;
+		int valo1= ++numero; //Operador incremento
+		int valo2= --numero; //Operador Decremento
+		System.out.println("el valor inicial es "+numero+" y es valor incrementado en uno es "+ valo1);
+		System.out.println("el valor inicial es "+numero+" y es valor decrementado en uno es "+ --numero);
+	
+		System.out.println("DECLARACION DE CONSTANTES");
+		final double pulgadas=2.54;
+		double cm=6;
+		double result= cm/pulgadas;
+		System.out.println("En "+cm+" centimetros hay "+result+" pulgadas");
+	
+	}
+
+}
