fb337624215c19b7eb833080314083ae267d5231
Francisco Hernández Castillo <48812779+frahercas@users.noreply.github.com>
2023-07-14 20:09:03 -0600

    Update A001Variables.java

--- Basicos de JAVA/A001Variables.java
+++ Basicos de JAVA/A001Variables.java
@@ -2,8 +2,8 @@
 public class A001Variables {
 
 	public static void main(String[] args) {
-		byte edad;		//Declaracion de variable
-		edad=25;		//inicalizacion de la variable
+		byte edad;		  //Declaracion de variable
+		edad=25;		  //inicalizacion de la variable
 		String name= "francisco"; //Declaracion e inicializacion de una variable en la misma linea
 		System.out.println(edad);
 		System.out.println(name);
