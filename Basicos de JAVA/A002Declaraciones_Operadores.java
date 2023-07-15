ad8b62b0f7d0b21c8f0643ea6aa77a7f350a77b4
Francisco Hernández Castillo <48812779+frahercas@users.noreply.github.com>
2023-07-14 20:10:55 -0600

    Update A002Declaraciones_Operadores.java

--- Basicos de JAVA/A002Declaraciones_Operadores.java
+++ Basicos de JAVA/A002Declaraciones_Operadores.java
@@ -4,15 +4,15 @@ public class A002Declaraciones_Operadores {
 	public static void main(String[] args) {
 		int a=5, b=7;
 		System.out.println("OPERADORES ARITMETICOS");
-		int suma=a+b; //Operador suma
-		int resta=a-b;	//Operador Resta
+		int suma=a+b;        //Operador suma
+		int resta=a-b;	     //Operador Resta
 		int multiplicacion= a*b;	//Operador multiplicacion
 		float x=7,y=2;
-		float division= x/y; 	//Operador division
+		float division= x/y; 	        //Operador division
 		System.out.println("suma igual a "+suma);
 		System.out.println("resta igual a "+resta);
 		System.out.println("multiplicacion igual a "+multiplicacion);
-		System.out.println("division igual a "+division);   //En estos ejemplos concatenamos una cadena de caracteres con el valor de una variable
+		System.out.println("division igual a "+division);        //En estos ejemplos concatenamos una cadena de caracteres con el valor de una variable
 		
 		System.out.println("OPERADORES incremento y decremento");
 		int numero= 100;
@@ -22,7 +22,7 @@ public class A002Declaraciones_Operadores {
 		System.out.println("el valor inicial es "+numero+" y es valor decrementado en uno es "+ --numero);
 	
 		System.out.println("DECLARACION DE CONSTANTES");
-		final double pulgadas=2.54;
+		final double pulgadas=2.54; //Declaracion de una constante propia
 		double cm=6;
 		double result= cm/pulgadas;
 		System.out.println("En "+cm+" centimetros hay "+result+" pulgadas");
